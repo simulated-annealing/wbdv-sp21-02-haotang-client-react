49e06142af8051ea912801f1c4f51cd42af95946
tanghao <tang.hao2@husky.neu.edu>
2021-03-24 01:18:58 -0700

    polish paragraph

--- src/components/course-editor/widgets/paragraph-widget.js
+++ src/components/course-editor/widgets/paragraph-widget.js
@@ -7,12 +7,14 @@ export const ParagraphWidget = ({
 
     return (<>
     {
-        editing &&
+        editing && <div className="mb-3">
+        <label className="form-label colorViolet fontBold" for="widgetTextarea"> Paragraph </label>
         <textarea id="widgetTextarea" value={cached.text} className="form-control" rows="8" onChange = {e => 
             setCached({
                 ...cached,
                 text: e.target.value
             })}/>
+        </div>
     }
     { !editing && <p>{cached.text}</p> }
     </>)
